5264bc823d04297e76e1f5d055ab760372e2a181
Nishant Rajput <nishant.rajput@pocketfm.com>
2022-12-13 19:51:50 +0530

    text deteciton

--- main/views.py
+++ main/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from utils import helper
+
+def search_view(request):
+    keyword = request.POST.get('keyword')
+    file_type = request.POST.get('file_type')
+    file_ = request.FILES.get('file')
+    language = request.POST.get('language')
+    face = request.FILES.get("face_search")
+
+
+    return HttpResponse('Success')
+
 
 def checking(request):
     return render(request,"index.html")
